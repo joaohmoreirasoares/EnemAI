f2f7e2d80df51cc5ebf2968f309c8e3e619f5819
[dyad] <joaohmoreirasoares@gmail.com>
2025-10-11 15:07:50 -0300

    [dyad] Fixing syntax error by removing indentation - wrote 1 file(s)

--- src/components/ui/scroll-card.tsx
+++ src/components/ui/scroll-card.tsx
@@ -1,7 +1,7 @@
 <p className="text-center">{card.description}</p>
                     <a
                       href={card.link}
-                      target='_blank'
+                      target="_blank"
                       className="w-fit bg-black p-3 rounded-md cursor-pointer text-white mx-auto block text-center"
                     >
                       {card.linkText}
