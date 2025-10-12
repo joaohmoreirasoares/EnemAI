6147de0f6d60b57fdd31248b2de91d87ef4b7b8f
[dyad] <joaohmoreirasoares@gmail.com>
2025-10-11 22:06:01 -0300

    Updated src/components/ui/scroll-card.tsx

--- src/components/ui/scroll-card.tsx
+++ src/components/ui/scroll-card.tsx
@@ -51,7 +51,7 @@ const Component = forwardRef<HTMLElement>((props, ref) => {
       <main className='bg-black' ref={ref}>
         <div className='wrapper'>
           <section className='text-white h-screen w-full bg-slate-950 grid place-content-center sticky top-0'>
-            <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
+            <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'>inunij</div>
 
 
           </section>
