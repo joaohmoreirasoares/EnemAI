74a6d4e75238491564307cb069f9ec197bf3c5df
[dyad] <joaohmoreirasoares@gmail.com>
2025-10-11 22:18:06 -0300

    [dyad] Changed Card Stack background to #572254 - wrote 1 file(s)

--- src/components/card-stack.ts
+++ src/components/card-stack.ts
@@ -35,15 +35,15 @@ export const CardStack: React.FC<CardStackProps> = ({ items, className, onCardCl
   const handleNext = () => {
     if (isAnimating) return;
     setIsAnimating(true);
-    setActiveIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
+    setActiveIndex((prev) => (prev === items.length - 1 ? 0 : prev - 1));
     setTimeout(() => setIsAnimating(false), 300);
   };
 
   return (
     <div className={cn("relative w-full h-full", className)}>
-      {/* Main container with new background */}
-      <div className="relative w-full h-full bg-gradient-to-b from-[#111] to-[#000] rounded-2xl overflow-hidden">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%239C92AC" fill-opacity="0.05"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20"></div>
+      {/* Main container with new background color */}
+      <div className="relative w-full h-full bg-[#572254] rounded-2xl overflow-hidden">
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.05"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20"></div>
         
         <div className="relative w-full h-full p-8">
           <AnimatePresence mode="wait">
